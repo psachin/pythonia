9fee2191ee5efcf4beaf5f96663d3c892d02f61e
sachin <iclcoolster@gmail.com>
2016-01-05 10:52:09 +0530

    Working copy with validated pylint

--- fuse/fusedate.py
+++ fuse/fusedate.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
-
+'''Simplest custom FS hierarchy with a file(date) within a root
+directory(fuse-test/)
+'''
 import datetime
 
 from fuse import FUSE, Operations, LoggingMixIn
 from stat import S_IFDIR, S_IFREG
-from sys import argv, exit
+from sys import argv
 from time import time
 
 
 class Context(LoggingMixIn, Operations):
+    '''Basically we need three actions
+    1. getattr - Get/Set file and directory attributes
+    2. read - Returns a date string when file(date) is read
+    3. readdir - a directory structure
+    .. rest all are set to None
+
+    Current system time is set as create-time, modified-time and
+    access-time
+    '''
     def getattr(self, path, fh=None):
-        pass
+        if path == '/':
+            attr = dict(st_mode=(S_IFDIR | int('0750', 8)), st_nlink=2)
+        elif path == '/date':
+            attr = dict(st_mode=(S_IFREG | int('0755', 8)), st_size=30)
+
+        attr['st_ctime'] = attr['st_mtime'] = attr['st_atime'] = time()
+        return attr
 
     def read(self, path, size, offset, fh):
-        pass
+        if path == '/date':
+            return datetime.datetime.now().strftime('%B %d, %Y') + '\n'
 
     def readdir(self, path, fh):
-        pass
+        return ['.', '..', 'date']
 
     access = None
     flush = None
@@ -33,3 +51,5 @@ if __name__ == '__main__':
     if len(argv) != 2:
         print('Usage: {} <mountpoint>'.format(argv[0]))
         exit(1)
+
+    PY_FUSE = FUSE(Context(), argv[1], foreground=True, ro=True)
